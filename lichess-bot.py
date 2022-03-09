e16816c1804d4e9d6f66c7cc57e2ff25a400057e
HeroRebor <96386317+HeroRebor@users.noreply.github.com>
2022-03-09 13:59:01 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -233,8 +233,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         def __init__(self, room):
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    conversation.send_reply(SendLine('player'), f'Good Luck @{opponent}')
-    conversation.send_reply(SendLine('spectator'), f'Good Luck @{opponent}')
+    conversation.send_reply(SendLine('player'), f'Hello this is bot made by @Soloboy4,Good Luck @{opponent}, Type !help for a list of command , I may reply too')
+    conversation.send_reply(SendLine('spectator'), f'Thanks a lot @{spectator} for watching this game')
     
     variant=game.perf_name
 
